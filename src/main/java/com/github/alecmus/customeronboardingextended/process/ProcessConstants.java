d20b4b8e5b4441e5e943ca7daaee90d5e5fa2593
Alec Musasa <alecmus@live.com>
2024-01-11 11:07:09 +0200

    Added ProcessConstants

---
+++ src/main/java/com/github/alecmus/customeronboardingextended/process/ProcessConstants.java
@@ -0,0 +1,8 @@
+package com.github.alecmus.customeronboardingextended.process;
+
+public class ProcessConstants {
+
+    public static final String VAR_SCORING_REQUEST_ID = "scoringRequestId";
+    public static final String VAR_SCORING_RETRY_COUNT = "scoringRetryCount";
+    public static final String VAR_SCORING_RESULT = "scoringResult";
+}
