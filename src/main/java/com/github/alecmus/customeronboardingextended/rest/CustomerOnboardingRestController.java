105cf62e632ffdd7a2925b6488664900a616b2f1
Alec Musasa <alecmus@live.com>
2024-01-11 13:54:40 +0200

    Added PUT mapping for /customer endpoint

---
+++ src/main/java/com/github/alecmus/customeronboardingextended/rest/CustomerOnboardingRestController.java
@@ -0,0 +1,59 @@
+package com.github.alecmus.customeronboardingextended.rest;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.HashMap;
+
+@RestController
+public class CustomerOnboardingRestController {
+
+    private static Logger logger = LoggerFactory.getLogger(CustomerOnboardingRestController.class);
+
+    @Autowired
+    private ZeebeClient client;
+
+    @PutMapping("/customer")
+    public ResponseEntity<CustomerOnboardingResponse> onboardCustomer(ServerWebExchange exchange) {
+        logger.info("PUT request received at /customer");
+
+        String paymentType = exchange.getRequest().getQueryParams().getFirst("paymentType");
+        if (paymentType==null) {paymentType = "prepaid";}
+        String monthlyPayment = exchange.getRequest().getQueryParams().getFirst("monthlyPayment");
+        if (monthlyPayment==null) {monthlyPayment = "45";}
+        String customerRegionScore = exchange.getRequest().getQueryParams().getFirst("customerRegionScore");
+        if (customerRegionScore==null) {customerRegionScore = "40";}
+
+        logger.info("Onboarding customer");
+
+        onboardCustomer(paymentType, Integer.parseInt(monthlyPayment), Integer.parseInt(customerRegionScore));
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+    public long onboardCustomer(String paymentType, int monthlyPayment, int customerRegionScore) {
+        HashMap<String, Object> variables = new HashMap<String, Object>();
+        variables.put("paymentType", paymentType);
+        variables.put("monthlyPayment", monthlyPayment);
+        variables.put("customerRegionScore", customerRegionScore);
+
+        logger.info("Creating new instance of process (blocking)");
+
+        ProcessInstanceEvent processInstance = client.newCreateInstanceCommand() //
+                .bpmnProcessId("customer-onboarding-extended") //
+                .latestVersion() //
+                .variables(variables) //
+                .send().join();
+        return processInstance.getProcessInstanceKey();
+    }
+
+    public static class CustomerOnboardingResponse {
+    }
+}
