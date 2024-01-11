92b1da32b92b9c1921d4e2538f60ab4d833eda6c
Alec Musasa <alecmus@live.com>
2024-01-11 10:59:52 +0200

    Added RestTemplateBuilder and RestTemplate beans

--- src/main/java/com/github/alecmus/customeronboardingextended/CustomerOnboardingExtendedApplication.java
+++ src/main/java/com/github/alecmus/customeronboardingextended/CustomerOnboardingExtendedApplication.java
@@ -3,6 +3,10 @@ package com.github.alecmus.customeronboardingextended;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Deployment(resources =  {"classpath*:*.bpmn", "classpath*:*.dmn"})
@@ -12,4 +16,15 @@ public class CustomerOnboardingExtendedApplication {
 		SpringApplication.run(CustomerOnboardingExtendedApplication.class, args);
 	}
 
+	// create RestTemplateBuilder bean if it doesn't already exist
+	@Bean
+	@ConditionalOnMissingBean(RestTemplateBuilder.class)
+	public RestTemplateBuilder restTemplateBuilder() {
+		return new RestTemplateBuilder();
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
 }
