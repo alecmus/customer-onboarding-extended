8ca6481119cdf817a31fb1b15a25181b45fd4baf
Alec Musasa <alecmus@live.com>
2024-01-11 10:54:51 +0200

    Automatic deployment of all models and DMNs

--- src/main/java/com/github/alecmus/customeronboardingextended/CustomerOnboardingExtendedApplication.java
+++ src/main/java/com/github/alecmus/customeronboardingextended/CustomerOnboardingExtendedApplication.java
@@ -1,9 +1,11 @@
 package com.github.alecmus.customeronboardingextended;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Deployment(resources =  {"classpath*:*.bpmn", "classpath*:*.dmn"})
 public class CustomerOnboardingExtendedApplication {
 
 	public static void main(String[] args) {
